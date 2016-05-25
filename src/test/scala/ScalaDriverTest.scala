587ba53371997f9dd08bec7dbae0ac2b4be6ab9e
alexandrepichon <alexandre.pichon.ap@gmail.com>
2016-05-25 10:52:46 +0200

    write concern

--- src/test/scala/ScalaDriverTest.scala
+++ src/test/scala/ScalaDriverTest.scala
@@ -8,6 +8,7 @@ import org.mongodb.scala._
 
 import scala.util.Success
 import scala.util.Failure
+import scala.util.Try
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -41,24 +42,41 @@ class ScalaDriverTest extends JUnitSuite with Matchers {
   @Test def insert {
     new UsersCollection {
       val doc = Document("""{name: 'bob'}""")
-      val observable = usersCollection.insertOne(doc)
-      observable.subscribe(new PrintObserver[Completed]())
+      usersCollection.insertOne(doc) should replyCompleted
     }
   }
 
   @Test def findOne {
     new UsersCollection {
       val observable = usersCollection.find()
-      observable.subscribe(new PrintObserver[Document]())
-    }
-  }
-
-  @Test def future {
-    new UsersCollection {
-      val observable = usersCollection.find()
       val future = observable.toFuture
       val res = Await.result(future, 100 millis)
       res should not be('empty)
+    }
+  }
+
+  @Test def writeConcern {
+    val collection = db.getCollection("users").withWriteConcern(WriteConcern.UNACKNOWLEDGED)
+    val doc = Document("""{name: 'UNACKNOWLEDGED'}""")
+    val observable = collection.insertOne(doc)
+    val future = observable.toFuture
+    val res = Await.result(future, 100 millis)
+    res.head should be(Completed())
+  }
+
+  private def replyCompleted = new org.scalatest.matchers.Matcher[Observable[Completed]] {
+    def apply(observable: Observable[Completed]) = {
+      val res: Try[Boolean] = Try {
+        Await.result(observable.toFuture, 100 millis).head == Completed()
+      }
+      org.scalatest.matchers.MatchResult(
+        res.isSuccess,
+        res match {
+          case Failure(e) => res + " was not a success " + res.asInstanceOf[Failure[Throwable]].get.getStackTrace.toString
+          case _ => ""
+        },
+        observable + " is completed"
+      )
     }
   }
 
